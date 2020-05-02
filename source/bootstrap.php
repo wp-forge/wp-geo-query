0341aff361752ac7579b977c856d5960c8db03e8
Micah Wood <micah@wpscholar.com>
2020-05-02 09:06:22 -0400

    Ensure library doesn't load more than once

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -1,7 +1,13 @@
 <?php
 
-if ( function_exists( 'add_action' ) ) {
+if ( ! function_exists( 'wpforge_initialize_wp_geo_query' ) ) {
 
-	add_action( 'after_setup_theme', array( 'WP_Forge\\GeoQuery\\GeoQuery', 'hooks' ) );
+	function wpforge_initialize_wp_geo_query() {
+		add_action( 'after_setup_theme', array( 'WP_Forge\\GeoQuery\\GeoQuery', 'hooks' ) );
+	}
+
+	if ( function_exists( 'add_action' ) ) {
+		wpforge_initialize_wp_geo_query();
+	}
 
 }
