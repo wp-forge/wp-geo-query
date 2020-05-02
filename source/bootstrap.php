a1c4c05ad3254c33597276a6de968eca1da60737
Micah Wood <micah@wpscholar.com>
2020-05-02 09:11:17 -0400

    Add comment for initialization function

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -2,6 +2,9 @@
 
 if ( ! function_exists( 'wpforge_initialize_wp_geo_query' ) ) {
 
+	/**
+	 * Initialize GeoQuery
+	 */
 	function wpforge_initialize_wp_geo_query() {
 		add_action( 'after_setup_theme', array( 'WP_Forge\\GeoQuery\\GeoQuery', 'hooks' ) );
 	}
